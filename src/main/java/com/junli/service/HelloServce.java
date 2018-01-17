579db2d3b549d2bf64a6a4e02c904729e8b14f8f
lijun <lijun@xkeshi.com>
2018-01-17 15:09:44 +0800

    feign 公用接口和实体

---
+++ src/main/java/com/junli/service/HelloServce.java
@@ -0,0 +1,23 @@
+package com.junli.service;
+
+import com.junli.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lijun
+ * @time 2018-01-16 15:26
+ */
+
+@RequestMapping("refactor")
+public interface HelloServce {
+
+    @GetMapping("/hello4")
+    String hello(@RequestParam("name") String name);
+
+    @GetMapping("/hello5")
+    User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
+
+
+    @PostMapping("/hello6")
+    String hello(@RequestBody User user);
+}
